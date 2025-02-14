18d071e65e825d2a77f13b482ce098bd61d4048a
Hector Garza <hectorg@smartxchain.com>
2025-02-14 16:23:14 -0500

    feat: add admin for process and methods

--- app/process/admin.py
+++ app/process/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import ProcessStep, Process
 
-# Register your models here.
+
+@admin.register(ProcessStep)
+class ProcessStepAdmin(admin.ModelAdmin):
+    list_display = ('process', 'method', 'step_number')
+
+
+@admin.register(Process)
+class ProcessAdmin(admin.ModelAdmin):
+    list_display = ('standard', 'classification', 'description')
