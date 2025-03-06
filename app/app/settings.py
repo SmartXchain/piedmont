7db14deceabb783b9427a4b538dfa69773e2cd70
Hector Garza <hectorg@smartxchain.com>
2025-03-06 10:24:03 -0500

    fix: PM sop

--- app/app/settings.py
+++ app/app/settings.py
@@ -134,6 +134,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'pm/static'),
+]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
