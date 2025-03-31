206345a970f02fd3375be6c9566b09a31fe9b862
Hector Garza <hectorg@smartxchain.com>
2025-03-31 17:35:50 -0400

    fix: collectstatic

--- app/staticfiles/staticfiles/pm/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-def pm_landing_page(request):
-    """Landing page for the Preventive Maintenance (PM) system."""
-    return render(request, 'pm/pm_landing.html')
