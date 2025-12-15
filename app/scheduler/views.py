c18f84f56aef3c24bb853881303700c3241f030a
Hector Garza <hectorg@smartxchain.com>
2025-12-15 12:51:18 -0500

    fix: 5 days view

--- app/scheduler/views.py
+++ app/scheduler/views.py
@@ -132,3 +132,4 @@ class AddDelayView(View):
             return JsonResponse({"status": "success"})
         except Exception as e:
             return JsonResponse({"status": "error", "message": str(e)}, status=400)
+
